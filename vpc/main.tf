314d395bedec8571cbaeae706bbe438aece77ca8
your name <your email>
2026-04-04 22:40:10 -0600

    2

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,14 +1,13 @@
-module "vpc"{
-source = "trialxphjx0.jfrog.io/tf-cloudops/vpc/local"
-version = "1.0.0"
+module "vpc" {
+  source = "./vpc"
 
-private_cidr = var.private_cidr
-availability_zones = var.availability_zones
-public_cidr = var.public_cidr
-environment = var.environment
-cidr_block = var.cidr_block
-log_destination = var.log_destination
-log_destination_type = var.log_destination_type
-vpc_name = var.vpc_name
-region = var.region
+  private_cidr = var.private_cidr
+  availability_zones = var.availability_zones
+  public_cidr = var.public_cidr
+  environment = var.environment
+  cidr_block = var.cidr_block
+  log_destination = var.log_destination
+  log_destination_type = var.log_destination_type
+  vpc_name = var.vpc_name
+  region = var.region
 }
